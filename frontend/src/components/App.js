fca59a11fa7ab526b70714434b5f83accd17b9f7
AleksandrDudik <saimonnightwalker@gmail.com>
2023-08-23 12:00:36 +0300

    First try 15

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 ﻿import React from "react";
-import "../index.css";
+import { Route, Switch, useHistory } from "react-router-dom";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
@@ -10,6 +10,13 @@ import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import AddPlacePopup from "./AddPlacePopup";
 import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import success from "../images/success.svg";
+import failure from "../images/failure.svg";
+import Login from "./Login";
+import Register from "./Register";
+import * as auth from "../utils/Auth";
+import InfoTooltip from "./InfoTooltip";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default App;
 
@@ -21,15 +28,22 @@ function App() {
   const [currentUser, setCurrentUser] = React.useState({});
   const [selectedCard, setSelectedCard] = React.useState(null);
   const [cards, setCards] = React.useState([]);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = React.useState(false);
+  const history = useHistory();
+  const [message, setMessage] = React.useState({ img: '', text: '' });
 
   React.useEffect(() => {
-    Promise.all([api.getApiUserInfo(), api.getCards()])
-      .then(([user, card]) => {
-        setCurrentUser(user);
-        setCards(card);
-      })
-      .catch((err) => alert(err));
-  }, []);
+    if ( loggedIn ) {
+      Promise.all([api.getApiUserInfo({ token: localStorage.jwt }), api.getCards({ token: localStorage.jwt })])
+        .then(([user, card]) => {
+          setCurrentUser(user);
+          setCards(card);
+        })
+        .catch((err) => alert(err));
+    }
+  }, [loggedIn]);
 
   function handleAddPlaceSubmit(cardData) {
     api.postCards(cardData)
@@ -78,12 +92,13 @@ function App() {
     setIsProfilePopupOpen(false);
     setIsAddPlacePopupOpen(false);
     setIsAvatarPopupOpen(false);
+    setIsInfoTooltipOpen(false);
     setSelectedCard(null);
   }
 
   function handleCardDelete(card) {
     api.deleteCard(card._id)
-      .then(() => {setCards(cards.filter((item) => item !== card))})
+      .then(() => {setCards((cards) => cards.filter((item) => item !== card))})
       .catch((err) => console.log(err));
   }
 
@@ -94,7 +109,7 @@ function App() {
       .catch((err) => console.log(err));
   }
 
-  const isOpen = isAvatarPopupOpen || isProfilePopupOpen || isAddPlacePopupOpen || selectedCard;
+  const isOpen = isAvatarPopupOpen || isProfilePopupOpen || isAddPlacePopupOpen || isInfoTooltipOpen || selectedCard;
 
   React.useEffect(() => {
 
@@ -120,18 +135,86 @@ function App() {
     }
   }, [isOpen]);
 
+  function handleAuth(password, email) {
+    auth.authorize(password, email)
+      .then((data) => {
+        setLoggedIn(true);
+        localStorage.setItem('jwt', data.token);
+        history.push('/');
+        setEmail(email);
+      })
+      .catch((err) => {
+        if (err.status === 400) {
+          setMessage({ img: failure, text: 'Не передано одно из полей'});
+        } else if (err.status === 401) {
+          setMessage({ img: failure, text: `Пользователь с email ${email} не найден`});
+        } else {
+          setMessage({ img: failure, text: 'Что-то пошло не так! Попробуйте ещё раз'});
+        }
+      })
+      .finally(() => setIsInfoTooltipOpen(true));
+  }
+
+  function handleRegistration(password, email) {
+    auth.register(password, email)
+      .then((res) => {
+        setEmail(res.data.email);
+        setMessage({ img: success, text: 'Вы успешно зарегистрировались!' });
+        history.push('/sign-in');
+      })
+      .catch(() => setMessage({ img: failure, text: 'Некорректно заполнено одно из полей' }))
+      .finally(() => setIsInfoTooltipOpen(true));
+  }
+
+  function onSignOut() {
+    localStorage.removeItem('jwt');
+    history.push('/sign-up');
+    setLoggedIn(false);
+  }
+
+  function tokenCheck() {
+    const jwt = localStorage.getItem('jwt');
+    if(jwt) {
+      auth.checkToken(jwt)
+        .then((res) => {
+          if(res) {
+            setLoggedIn(true);
+            setEmail(res.data.email);
+            history.push('/');
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  React.useEffect(() => {
+    tokenCheck();
+  }, []);
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className="page">
-        <Header />
-        <Main onEditProfile={handleEditProfileClick} onEditAvatar={handleEditAvatarClick} onAddPlace={handleAddPlaceClick} onCardClick={handleCardClick} onCardLike={handleCardLike}
-              onCardDelete={handleCardDelete} onClose={closeAllPopups} cards={cards} selectedCard={selectedCard} />
+        <Header loggedIn={loggedIn} email={email} onSignOut={onSignOut} />
+        <Switch>
+          <ProtectedRoute exact path='/' loggedIn={loggedIn} component={Main} onEditProfile={handleEditProfileClick} onEditAvatar={handleEditAvatarClick} 
+            onAddPlace={handleAddPlaceClick} onCardClick={handleCardClick} onCardLike={handleCardLike} onCardDelete={handleCardDelete} selectedCard={selectedCard} cards={cards} />
+          <Route path='/sign-up'>
+            <Login isOpen={isProfilePopupOpen} onAuth={handleAuth} />
+          </Route>
+          <Route path='/sign-in'>
+            <Register isOpen={isProfilePopupOpen} onRegister={handleRegistration} isInfoTooltipOpen={isInfoTooltipOpen} />
+          </Route>
+          <Route path='/'>
+            <Login isOpen={isProfilePopupOpen} onAuth={handleAuth} />
+          </Route>
+        </Switch>
         <Footer />
         <AddPlacePopup isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} onAddPlace={handleAddPlaceSubmit} />
         <EditAvatarPopup isOpen={isAvatarPopupOpen} onClose={closeAllPopups} onUpdateAvatar={handleUpdateAvatar} />
         <EditProfilePopup isOpen={isProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
         <ImagePopup onClose={closeAllPopups} card={selectedCard} />
         <PopupWithForm name="form-confirm" title="Вы уверены?" buttonText="Да" />
+        <InfoTooltip name='tooltip' isOpen={isInfoTooltipOpen} onClose={closeAllPopups} img={message.img} title={message.text} />
       </div>
     </CurrentUserContext.Provider>
   );
